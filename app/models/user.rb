07215835e1245d0aece4536113fa3cd76eae3a27
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-04-05 23:53:45 +0000

    user-specs implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
 class User < ActiveRecord::Base
+
+ 
+
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: { case_sensitive: true }
+
+  def self.authenticate_with_credentials(email, password)
+    # new_email = email.downcase
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
